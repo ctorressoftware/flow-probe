3094620ff52b13ed303ef47c7636dbaba846cb26
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-01 22:13:24 -0400

    feat: add commands manager to the project

---
+++ src/main/java/io/github/ctorressoftware/CommandManager.java
@@ -0,0 +1,25 @@
+package io.github.ctorressoftware;
+
+import picocli.CommandLine;
+
+import java.util.Map;
+
+@CommandLine.Command(name = "FlowProbe", version = "FlowProbe 1.0", mixinStandardHelpOptions = true)
+public class CommandManager implements Runnable {
+
+    @CommandLine.Option(
+            names = {"-f", "-file"},
+            required = true,
+            paramLabel = "FILE",
+            description = "file url"
+    )
+    String filePath;
+
+    @Override
+    public void run() {
+        YAMLProcessor yamlProcessor = new YAMLProcessor();
+        Map<String, Object> doc = yamlProcessor.read(filePath);
+        System.out.println(doc.keySet());
+        System.out.println(doc.values());
+    }
+}
