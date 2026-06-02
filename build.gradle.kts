b12e73a95d99b93b6f0c254a045349bcf4a5617e
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-01 22:12:42 -0400

    chore: add snakeyaml to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 
 dependencies {
     implementation("info.picocli:picocli:4.7.7")
+    implementation("org.yaml:snakeyaml:1.8")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
