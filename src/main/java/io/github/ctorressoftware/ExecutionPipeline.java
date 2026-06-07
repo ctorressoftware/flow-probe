6c1379e700289e68efd73a436e3f9927764a8dab
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-06 23:23:21 -0400

    feat: add ExecutionPipeline advances in the main logic of services execution

--- src/main/java/io/github/ctorressoftware/ExecutionPipeline.java
+++ src/main/java/io/github/ctorressoftware/ExecutionPipeline.java
@@ -1,4 +1,47 @@
 package io.github.ctorressoftware;
 
-public class ServiceExecutor {
+import io.github.ctorressoftware.exceptions.NoDefinedStepsException;
+import io.github.ctorressoftware.model.ExecutionContext;
+import io.github.ctorressoftware.model.Proxy;
+import io.github.ctorressoftware.model.RequestFormat;
+import io.github.ctorressoftware.model.StepFormat;
+
+import java.util.List;
+
+public class ExecutionPipeline {
+
+    private final Proxy proxy;
+    private final ExecutionContext context;
+
+    public ExecutionPipeline() {
+        this.proxy = new Proxy();
+        this.context = new ExecutionContext();
+    }
+
+    public void execute(List<StepFormat> steps) {
+
+        if (steps == null || steps.isEmpty()) {
+            throw new NoDefinedStepsException();
+        }
+
+        steps.forEach(step -> {
+
+            step.expect(); // Validate expectations
+
+            proxy.call(new RequestFormat(
+                    step.request().url(),
+                    step.request().method(),
+                    step.request().headers(),
+                    step.request().body()
+            ));
+
+            step.export(); // Export results to context
+
+        });
+    }
+
+    private String resolvePlaceholder() {
+
+        return "";
+    }
 }
