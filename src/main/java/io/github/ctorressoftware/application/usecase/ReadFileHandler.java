b96fe2fb284eed029fc57f13443ab849fa14c116
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-08 23:54:04 -0400

    feat: create ReadFileHandler to manage reading logic

---
+++ src/main/java/io/github/ctorressoftware/application/usecase/ReadFileHandler.java
@@ -0,0 +1,24 @@
+package io.github.ctorressoftware.application.usecase;
+
+import io.github.ctorressoftware.application.port.in.readfile.ReadFileCommand;
+import io.github.ctorressoftware.application.port.in.readfile.ReadFileResult;
+import io.github.ctorressoftware.application.port.in.readfile.ReadFileUseCase;
+import io.github.ctorressoftware.application.port.out.FlowFileReader;
+import io.github.ctorressoftware.domain.model.StepFormat;
+
+import java.util.List;
+
+public class ReadFileHandler implements ReadFileUseCase {
+
+    private final FlowFileReader flowFileReader;
+
+    public ReadFileHandler(FlowFileReader flowFileReader) {
+        this.flowFileReader = flowFileReader;
+    }
+
+    @Override
+    public ReadFileResult read(ReadFileCommand command) {
+        List<StepFormat> list = flowFileReader.read(command.filePath());
+        return new ReadFileResult(list);
+    }
+}
