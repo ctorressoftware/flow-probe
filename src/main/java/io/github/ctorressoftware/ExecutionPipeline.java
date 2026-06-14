ef7fc03261776efbcc48aafd5ea6cd86eeedeea1
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-14 12:54:20 -0400

    refactor: adapt ExecutionPipeline file to the new dependencies files

--- src/main/java/io/github/ctorressoftware/ExecutionPipeline.java
+++ src/main/java/io/github/ctorressoftware/ExecutionPipeline.java
@@ -1,12 +1,14 @@
 package io.github.ctorressoftware;
 
-import io.github.ctorressoftware.exceptions.NoDefinedStepsException;
-import io.github.ctorressoftware.model.ExecutionContext;
-import io.github.ctorressoftware.model.Proxy;
-import io.github.ctorressoftware.model.RequestFormat;
-import io.github.ctorressoftware.model.StepFormat;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.ctorressoftware.domain.model.*;
+import io.github.ctorressoftware.domain.exception.NoDefinedStepsException;
+import io.github.ctorressoftware.infrastructure.context.ExecutionContext;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ExecutionPipeline {
 
@@ -18,30 +20,68 @@ public class ExecutionPipeline {
         this.context = new ExecutionContext();
     }
 
-    public void execute(List<StepFormat> steps) {
+    public ExecutionResume execute(String flowName, List<FlowStep> steps) {
 
-        if (steps == null || steps.isEmpty()) {
+        ExecutionResume executionResume = ExecutionResume.create(flowName);
+
+        if (steps == null || steps.isEmpty())
             throw new NoDefinedStepsException();
-        }
 
         steps.forEach(step -> {
 
-            step.expect(); // Validate expectations
+            ExecutionResumeDetail stepDetails = ExecutionResumeDetail.create(step.getStepName());
+            ServiceCall request = step.getServiceCall();
 
-            proxy.call(new RequestFormat(
-                    step.request().url(),
-                    step.request().method(),
-                    step.request().headers(),
-                    step.request().body()
+            if (step.getExpect() != null) {
+                request = resolvePlaceholders(request);
+            }
+
+            var response = proxy.call(new ServiceCall(
+                    request.url(),
+                    request.method(),
+                    request.headers(),
+                    request.body()
             ));
 
-            step.export(); // Export results to context
+            if (response == null || response.status() != 200) {
+                stepDetails.setSuccessful(false);
+                stepDetails.setResponseString(null);
+            }
 
+            String jsonResponse = Objects.requireNonNull(response).responseBody();
+
+            if (step.getExport() != null) {
+
+                ObjectMapper mapper = new ObjectMapper();
+
+                try {
+                    JsonNode root = mapper.readTree(jsonResponse);
+                    step.getExport().forEach((key, keyValue) -> {
+                        String valuePath = "/" + keyValue.replace(".", "/");
+                        String value = root.at(valuePath).asText();
+                        context.putVariable(key, value);
+                    });
+
+                    stepDetails.setSuccessful(true);
+                    stepDetails.setResponseString(jsonResponse);
+                    executionResume.getStepsResults().add(stepDetails);
+
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         });
+
+        return executionResume;
     }
 
-    private String resolvePlaceholder() {
+    private ServiceCall resolvePlaceholders(ServiceCall request) {
 
-        return "";
+        return new ServiceCall(
+                context.resolvePlaceholders(request.url()),
+                request.method(),
+                request.headers(),
+                context.resolvePlaceholders(request.body().toString())
+        );
     }
 }
