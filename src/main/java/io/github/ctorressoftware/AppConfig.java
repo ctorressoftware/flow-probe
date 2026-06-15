9510de45a6fff7ec4e0b09c2e357773a0aa28937
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-14 21:09:27 -0400

    feat: add AppConfig to create singleton objects

---
+++ src/main/java/io/github/ctorressoftware/AppConfig.java
@@ -0,0 +1,15 @@
+package io.github.ctorressoftware;
+
+import io.github.ctorressoftware.application.port.in.readfile.ReadFileUseCase;
+import io.github.ctorressoftware.application.port.out.FlowFileReader;
+import io.github.ctorressoftware.application.usecase.ReadFileHandler;
+import io.github.ctorressoftware.infrastructure.entrypoint.filereader.YAMLReader;
+
+public final class AppConfig {
+    private final FlowFileReader flowFileReader = new YAMLReader();
+    private final ReadFileUseCase readFileUseCase = new ReadFileHandler(flowFileReader);
+
+    public ReadFileUseCase readFileUseCase() {
+        return readFileUseCase;
+    }
+}
