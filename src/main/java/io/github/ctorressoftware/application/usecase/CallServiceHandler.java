6462f256696bd1b066618260ff830988ba065e58
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-16 17:32:53 -0400

    feat: add CallServiceHandler bootstrap with todo comment

---
+++ src/main/java/io/github/ctorressoftware/application/usecase/CallServiceHandler.java
@@ -0,0 +1,17 @@
+package io.github.ctorressoftware.application.usecase;
+
+import io.github.ctorressoftware.application.port.in.servicecalling.CallServiceCommand;
+import io.github.ctorressoftware.application.port.in.servicecalling.CallServiceResult;
+import io.github.ctorressoftware.application.port.in.servicecalling.CallServiceUseCase;
+
+public class CallServiceHandler implements CallServiceUseCase {
+
+    public CallServiceHandler() {}
+
+    // TODO: complete this implementation
+    @Override
+    public CallServiceResult call(CallServiceCommand command) {
+        return null;
+    }
+    
+}
