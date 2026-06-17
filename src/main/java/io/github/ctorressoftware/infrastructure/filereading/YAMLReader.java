4a6487b96f3c099888125b89c8f9b83b306c9c78
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-16 22:16:12 -0400

    refactor: relocate YAMLReader to a new package

--- src/main/java/io/github/ctorressoftware/infrastructure/filereading/YAMLReader.java
+++ src/main/java/io/github/ctorressoftware/infrastructure/filereading/YAMLReader.java
@@ -1,4 +1,4 @@
-package io.github.ctorressoftware.infrastructure.entrypoint.filereader;
+package io.github.ctorressoftware.infrastructure.filereading;
 
 import io.github.ctorressoftware.application.port.out.FlowFileReader;
 import io.github.ctorressoftware.domain.model.FilePath;
