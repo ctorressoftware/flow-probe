738b2572654b8d4f518a3e0207a86525030b8164
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-16 23:43:41 -0400

    feat: add ExecuteFlowHandler to usecase package

---
+++ src/main/java/io/github/ctorressoftware/application/usecase/flowexecution/FlowExecutor.java
@@ -0,0 +1,7 @@
+package io.github.ctorressoftware.application.usecase.flowexecution;
+
+/* TODO: migrate ExecutionPipeline logic to this file
+ * Set ExecutionPipeline as an orchestrator and delegate other logics to helper files
+ */
+public class FlowExecutor {
+}
