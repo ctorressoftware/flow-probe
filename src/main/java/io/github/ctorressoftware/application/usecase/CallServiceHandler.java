f2d45fb07be816230a486d29d661b83d2971842e
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-16 22:23:11 -0400

    feat: implement call method in CallServiceHandler using ServiceCaller out-port abstraction

--- src/main/java/io/github/ctorressoftware/application/usecase/CallServiceHandler.java
+++ src/main/java/io/github/ctorressoftware/application/usecase/CallServiceHandler.java
@@ -3,15 +3,20 @@ package io.github.ctorressoftware.application.usecase;
 import io.github.ctorressoftware.application.port.in.servicecalling.CallServiceCommand;
 import io.github.ctorressoftware.application.port.in.servicecalling.CallServiceResult;
 import io.github.ctorressoftware.application.port.in.servicecalling.CallServiceUseCase;
+import io.github.ctorressoftware.application.port.out.ServiceCaller;
+import io.github.ctorressoftware.domain.model.CallResult;
 
 public class CallServiceHandler implements CallServiceUseCase {
 
-    public CallServiceHandler() {}
+    private final ServiceCaller serviceCaller;
 
-    // TODO: complete this implementation
+    public CallServiceHandler(ServiceCaller serviceCaller) {
+        this.serviceCaller = serviceCaller;
+    }
+
     @Override
     public CallServiceResult call(CallServiceCommand command) {
-        return null;
+        CallResult result = serviceCaller.call(command.serviceCall());
+        return new CallServiceResult(result);
     }
-    
 }
