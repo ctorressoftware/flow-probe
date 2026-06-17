5fb8c51f84e94873d1057a5b8b61258743a77e89
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-16 23:43:35 -0400

    feat: add ExecuteFlowHandler to usecase package

---
+++ src/main/java/io/github/ctorressoftware/application/usecase/flowexecution/ExecuteFlowHandler.java
@@ -0,0 +1,13 @@
+package io.github.ctorressoftware.application.usecase.flowexecution;
+
+import io.github.ctorressoftware.application.port.in.flowexecution.ExecuteFlowCommand;
+import io.github.ctorressoftware.application.port.in.flowexecution.ExecuteFlowResult;
+import io.github.ctorressoftware.application.port.in.flowexecution.ExecuteFlowUseCase;
+
+public class ExecuteFlowHandler implements ExecuteFlowUseCase {
+
+    @Override
+    public ExecuteFlowResult execute(ExecuteFlowCommand command) {
+        return null;
+    }
+}
