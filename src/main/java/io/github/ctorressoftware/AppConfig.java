3db11d3948009392589630985b09d5900e3367c8
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-16 22:17:07 -0400

    refactor: re-import YAMLReader in AppConfig

--- src/main/java/io/github/ctorressoftware/AppConfig.java
+++ src/main/java/io/github/ctorressoftware/AppConfig.java
@@ -3,7 +3,7 @@ package io.github.ctorressoftware;
 import io.github.ctorressoftware.application.port.in.readfile.ReadFileUseCase;
 import io.github.ctorressoftware.application.port.out.FlowFileReader;
 import io.github.ctorressoftware.application.usecase.ReadFileHandler;
-import io.github.ctorressoftware.infrastructure.entrypoint.filereader.YAMLReader;
+import io.github.ctorressoftware.infrastructure.filereading.YAMLReader;
 
 public final class AppConfig {
     private final FlowFileReader flowFileReader = new YAMLReader();
