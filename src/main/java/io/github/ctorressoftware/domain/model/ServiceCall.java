7a61b7a2c9a38458ef2e742085140948281f421c
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-16 22:09:08 -0400

    chore: add todo comment in ServiceCall domain model

--- src/main/java/io/github/ctorressoftware/domain/model/ServiceCall.java
+++ src/main/java/io/github/ctorressoftware/domain/model/ServiceCall.java
@@ -2,6 +2,10 @@ package io.github.ctorressoftware.domain.model;
 
 import java.util.Map;
 
+/*
+ * TODO: ServiceCall is currently designed for HTTP-style calls.
+ * Revisit this design if FlowProbe needs to support other call types, such as gRPC over HTTP/2 or shell commands.
+ */
 public record ServiceCall(
         String url,
         String method,
