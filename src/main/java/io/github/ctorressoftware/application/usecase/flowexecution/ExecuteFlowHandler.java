d9edd5ea4998189ccc3260a4ab372aa50c6425e6
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-19 00:00:22 -0400

    feat: add ExecutionFlowHandler logic implementing FlowExecutor to delegate flow execution

--- src/main/java/io/github/ctorressoftware/application/usecase/flowexecution/ExecuteFlowHandler.java
+++ src/main/java/io/github/ctorressoftware/application/usecase/flowexecution/ExecuteFlowHandler.java
@@ -3,11 +3,21 @@ package io.github.ctorressoftware.application.usecase.flowexecution;
 import io.github.ctorressoftware.application.port.in.flowexecution.ExecuteFlowCommand;
 import io.github.ctorressoftware.application.port.in.flowexecution.ExecuteFlowResult;
 import io.github.ctorressoftware.application.port.in.flowexecution.ExecuteFlowUseCase;
+import io.github.ctorressoftware.domain.model.ExecutionResume;
 
 public class ExecuteFlowHandler implements ExecuteFlowUseCase {
 
+    private final FlowExecutor executor;
+
+    public ExecuteFlowHandler(FlowExecutor executor) {
+        this.executor = executor;
+    }
+
     @Override
     public ExecuteFlowResult execute(ExecuteFlowCommand command) {
-        return null;
+
+        ExecutionResume resume = executor.execute(command.flow());
+
+        return new ExecuteFlowResult(resume);
     }
 }
