3ab61142b4e8b51c40923bf0758ecf351b0b7ee3
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-21 21:05:45 -0400

    feat: define create method in ImpedimentTicketCreator port

--- src/main/java/io/github/ctorressoftware/application/port/out/ImpedimentTicketCreator.java
+++ src/main/java/io/github/ctorressoftware/application/port/out/ImpedimentTicketCreator.java
@@ -1,4 +1,7 @@
 package io.github.ctorressoftware.application.port.out;
 
+import io.github.ctorressoftware.domain.model.ImpedimentTicket;
+
 public interface ImpedimentTicketCreator {
+    void create(ImpedimentTicket ticket);
 }
