8489ce3bf8bda2a8a229e8918e6d2ea58efbd045
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-21 21:04:54 -0400

    feat: create AzureConfigure command bootstrap

---
+++ src/main/java/io/github/ctorressoftware/infrastructure/cli/AzureConfigureCommand.java
@@ -0,0 +1,15 @@
+package io.github.ctorressoftware.infrastructure.cli;
+
+import java.util.concurrent.Callable;
+
+public class AzureConfigureCommand implements Callable<Integer> {
+
+    public AzureConfigureCommand() {}
+
+    @Override
+    public Integer call() {
+        // TODO: to make azure-properties configuration
+        return 0;
+    }
+}
+
