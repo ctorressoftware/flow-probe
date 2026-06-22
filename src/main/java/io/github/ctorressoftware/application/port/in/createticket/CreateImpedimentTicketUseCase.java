1f4234e0f80c951c8c4495f06575218f3d8c5b0f
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-21 21:07:35 -0400

    feat: create usecase for CreateImpedimentTicket feature

---
+++ src/main/java/io/github/ctorressoftware/application/port/in/createticket/CreateImpedimentTicketUseCase.java
@@ -0,0 +1,5 @@
+package io.github.ctorressoftware.application.port.in.createticket;
+
+public interface CreateImpedimentTicketUseCase {
+    CreateImpedimentTicketResult createTicket(CreateImpedimentTicketCommand command);
+}
