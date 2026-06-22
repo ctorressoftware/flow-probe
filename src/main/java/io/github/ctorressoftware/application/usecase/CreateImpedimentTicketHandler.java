fffd1ca55e45b4de982a495d4a101f57f6a3bc51
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-21 21:13:39 -0400

    feat: create CreateImpedimentTicketHandler bootstrap

---
+++ src/main/java/io/github/ctorressoftware/application/usecase/CreateImpedimentTicketHandler.java
@@ -0,0 +1,12 @@
+package io.github.ctorressoftware.application.usecase;
+
+import io.github.ctorressoftware.application.port.in.createticket.CreateImpedimentTicketCommand;
+import io.github.ctorressoftware.application.port.in.createticket.CreateImpedimentTicketResult;
+import io.github.ctorressoftware.application.port.in.createticket.CreateImpedimentTicketUseCase;
+
+public class CreateImpedimentTicketHandler implements CreateImpedimentTicketUseCase {
+    @Override
+    public CreateImpedimentTicketResult createTicket(CreateImpedimentTicketCommand command) {
+        return null;
+    }
+}
