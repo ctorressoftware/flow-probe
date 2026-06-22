5c739387496f6dc71991bd07a48552b4e1448faf
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-21 21:12:51 -0400

    feat: create AzureDevOpsImpedimentTicketCreatorAdapter bootstrap

---
+++ src/main/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsImpedimentTicketCreatorAdapter.java
@@ -0,0 +1,11 @@
+package io.github.ctorressoftware.infrastructure.ticket.azuredevops;
+
+import io.github.ctorressoftware.application.port.out.ImpedimentTicketCreator;
+import io.github.ctorressoftware.domain.model.ImpedimentTicket;
+
+public class AzureDevOpsImpedimentTicketCreatorAdapter implements ImpedimentTicketCreator {
+    @Override
+    public void create(ImpedimentTicket ticket) {
+
+    }
+}
