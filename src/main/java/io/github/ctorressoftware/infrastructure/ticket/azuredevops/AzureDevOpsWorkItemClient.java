e3ce746a045e7a2928cddcbf4d4008cde678bfca
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-23 23:52:51 -0400

    feat: add http calling logic to test Azure DevOps API

--- src/main/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsWorkItemClient.java
+++ src/main/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsWorkItemClient.java
@@ -1,7 +1,12 @@
 package io.github.ctorressoftware.infrastructure.ticket.azuredevops;
 
+import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
 
+// TODO: This is a temporary logic to test Azure DevOps API. Refactor this later.
 public class AzureDevOpsWorkItemClient {
 
     private final HttpClient client;
@@ -12,5 +17,18 @@ public class AzureDevOpsWorkItemClient {
 
     public void createWorkItem(AzureDevOpsCreateWorkItemRequest request) {
 
+        String endpoint = "/" + AzureSettings.AZURE_ORGANIZATION 
+            + "/" + AzureSettings.AZURE_PROJECT 
+            + "/_apis/wit/workitems/impediment?api-version=7.2-preview.3";
+
+        URI uri = URI.create(AzureSettings.BASE_URL + endpoint);
+
+        HttpRequest httpRequest = HttpRequest.newBuilder(uri).build();
+
+        try {
+            client.send(httpRequest, BodyHandlers.ofString());   
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("error");
+        }
     }
 }
