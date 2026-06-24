08a4c4c744bf14657a1bbcbcf6c65aa228065da1
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-24 09:02:33 -0400

    feat: add Azure DevOps API client to handle api calls

--- src/main/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsWorkItemClient.java
+++ src/main/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsWorkItemClient.java
@@ -4,7 +4,12 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 // TODO: This is a temporary logic to test Azure DevOps API. Refactor this later.
 public class AzureDevOpsWorkItemClient {
@@ -15,18 +20,35 @@ public class AzureDevOpsWorkItemClient {
         this.client = HttpClient.newHttpClient();
     }
 
-    public void createWorkItem(AzureDevOpsCreateWorkItemRequest request) {
+    public String createWorkItem(AzureDevOpsCreateWorkItemRequest request) {
 
-        String endpoint = "/" + AzureSettings.AZURE_ORGANIZATION 
-            + "/" + AzureSettings.AZURE_PROJECT 
+        ObjectMapper mapper = new ObjectMapper();
+
+        String endpoint = AzureSettings.AZURE_ORGANIZATION + "/" + AzureSettings.AZURE_PROJECT 
             + "/_apis/wit/workitems/impediment?api-version=7.2-preview.3";
 
-        URI uri = URI.create(AzureSettings.BASE_URL + endpoint);
-
-        HttpRequest httpRequest = HttpRequest.newBuilder(uri).build();
-
         try {
-            client.send(httpRequest, BodyHandlers.ofString());   
+        
+            URI uri = URI.create(AzureSettings.BASE_URL + "/" + endpoint);
+            
+            String jsonBody = mapper.writeValueAsString(request);
+            
+            HttpRequest httpRequest = HttpRequest
+                .newBuilder(uri)
+                .POST(BodyPublishers.ofString(jsonBody))
+                .build();
+            
+            HttpResponse<String> response = client
+                .send(httpRequest, BodyHandlers.ofString());
+
+                if (response.statusCode() != 200) {
+                    throw new RuntimeException("error");
+                }
+
+                return response.body();
+
+        } catch(JsonProcessingException e) {
+            throw new RuntimeException("error");
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException("error");
         }
