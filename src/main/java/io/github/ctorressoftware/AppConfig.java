131a9b0d76b98d2ef4baedc82aa330759aa78483
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-25 23:53:47 -0400

    feat: create azure devops objects to test ticket creation feature

--- src/main/java/io/github/ctorressoftware/AppConfig.java
+++ src/main/java/io/github/ctorressoftware/AppConfig.java
@@ -1,15 +1,21 @@
 package io.github.ctorressoftware;
 
+import io.github.ctorressoftware.application.port.in.createticket.CreateImpedimentTicketUseCase;
 import io.github.ctorressoftware.application.port.in.flowexecution.ExecuteFlowUseCase;
 import io.github.ctorressoftware.application.port.in.readfile.ReadFileUseCase;
 import io.github.ctorressoftware.application.port.out.FlowFileReader;
+import io.github.ctorressoftware.application.port.out.ImpedimentTicketCreator;
 import io.github.ctorressoftware.application.port.out.ServiceCaller;
+import io.github.ctorressoftware.application.usecase.CreateImpedimentTicketHandler;
 import io.github.ctorressoftware.application.usecase.ReadFileHandler;
 import io.github.ctorressoftware.application.usecase.flowexecution.ExecuteFlowHandler;
 import io.github.ctorressoftware.application.usecase.flowexecution.FlowExecutor;
 import io.github.ctorressoftware.domain.model.Context;
 import io.github.ctorressoftware.infrastructure.callservice.RestServiceCaller;
 import io.github.ctorressoftware.infrastructure.readfile.YAMLReader;
+import io.github.ctorressoftware.infrastructure.ticket.azuredevops.AzureDevOpsImpedimentTicketCreatorAdapter;
+import io.github.ctorressoftware.infrastructure.ticket.azuredevops.AzureDevOpsWorkItemClient;
+import io.github.ctorressoftware.infrastructure.ticket.azuredevops.AzureDevOpsWorkItemTicketCreator;
 
 public final class AppConfig {
     private final FlowFileReader flowFileReader = new YAMLReader();
@@ -18,6 +24,10 @@ public final class AppConfig {
     private final ServiceCaller serviceCaller = new RestServiceCaller();
     private final FlowExecutor flowExecutor = new FlowExecutor(context, serviceCaller);
     private final ExecuteFlowUseCase executeFlowUseCase = new ExecuteFlowHandler(flowExecutor);
+    private final AzureDevOpsWorkItemClient azureDevOpsWorkItemClient = new AzureDevOpsWorkItemClient();
+    private final AzureDevOpsWorkItemTicketCreator azureDevOpsWorkItemTicketCreator = new AzureDevOpsWorkItemTicketCreator(azureDevOpsWorkItemClient);
+    private final ImpedimentTicketCreator impedimentTicketCreator = new AzureDevOpsImpedimentTicketCreatorAdapter(azureDevOpsWorkItemTicketCreator);
+    private final CreateImpedimentTicketUseCase createImpedimentTicketUseCase = new CreateImpedimentTicketHandler(impedimentTicketCreator);
 
     public ReadFileUseCase readFileUseCase() {
         return readFileUseCase;
@@ -25,5 +35,9 @@ public final class AppConfig {
 
     public ExecuteFlowUseCase executeFlowUseCase() {
         return executeFlowUseCase;
+    }
+
+    public CreateImpedimentTicketUseCase createImpedimentTicketUseCase() {
+        return createImpedimentTicketUseCase;
     }
 }
