3270936abe3143e06fedfe1ffffa73cc4a551e6b
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-27 00:00:08 -0400

    feat: add CreateImpedimentTicketHandler logic to test Azure ticket creation

--- src/main/java/io/github/ctorressoftware/application/usecase/CreateImpedimentTicketHandler.java
+++ src/main/java/io/github/ctorressoftware/application/usecase/CreateImpedimentTicketHandler.java
@@ -3,10 +3,21 @@ package io.github.ctorressoftware.application.usecase;
 import io.github.ctorressoftware.application.port.in.createticket.CreateImpedimentTicketCommand;
 import io.github.ctorressoftware.application.port.in.createticket.CreateImpedimentTicketResult;
 import io.github.ctorressoftware.application.port.in.createticket.CreateImpedimentTicketUseCase;
+import io.github.ctorressoftware.application.port.out.ImpedimentTicketCreator;
 
 public class CreateImpedimentTicketHandler implements CreateImpedimentTicketUseCase {
+
+    private final ImpedimentTicketCreator impedimentTicketCreator;
+
+    public CreateImpedimentTicketHandler(ImpedimentTicketCreator impedimentTicketCreator) {
+        this.impedimentTicketCreator = impedimentTicketCreator;
+    }
+
     @Override
     public CreateImpedimentTicketResult createTicket(CreateImpedimentTicketCommand command) {
+
+        impedimentTicketCreator.create(command.impedimentTicket());
+
         return null;
     }
 }
