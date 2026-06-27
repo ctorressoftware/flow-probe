ff0755e7e47032f352037aed00065b2fcbf4effb
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-26 23:50:54 -0400

    fix: adjust Azure DevOps request structure

--- src/main/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsCreateWorkItemRequest.java
+++ src/main/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsCreateWorkItemRequest.java
@@ -4,18 +4,14 @@ import io.github.ctorressoftware.domain.model.ImpedimentTicket;
 
 import java.util.List;
 
-public record AzureDevOpsCreateWorkItemRequest(
-        String workItemType,
-        List<AzureDevOpsPatchOperation> operations
-) {
+public record AzureDevOpsCreateWorkItemRequest(List<AzureDevOpsPatchOperation> operations) {
 
     public static AzureDevOpsCreateWorkItemRequest from(ImpedimentTicket ticket) {
         return new AzureDevOpsCreateWorkItemRequest(
-                "Product Backlog Item",
                 List.of(
                         AzureDevOpsPatchOperation.add(
                                 "/fields/System.Title",
-                                ticket.getDescription()
+                                ticket.getTitle()
                         ),
                         AzureDevOpsPatchOperation.add(
                                 "/fields/System.Description",
