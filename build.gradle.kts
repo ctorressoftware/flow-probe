364d93d5a9ac3379e103389033962f0f1485299b
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-28 12:48:49 -0400

    chore: add jackson-datatype-jsr310:2.22.0 dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("info.picocli:picocli:4.7.7")
     implementation("org.yaml:snakeyaml:1.8")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.22.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.22.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
