3b6404d92e13a6fdecc6c72eed213d22eacaff2e
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-28 01:50:58 -0400

    refactor: adjust create service definition adding ImpedimentTicket as the response object

--- src/main/java/io/github/ctorressoftware/application/port/out/ImpedimentTicketCreator.java
+++ src/main/java/io/github/ctorressoftware/application/port/out/ImpedimentTicketCreator.java
@@ -3,5 +3,5 @@ package io.github.ctorressoftware.application.port.out;
 import io.github.ctorressoftware.domain.model.ImpedimentTicket;
 
 public interface ImpedimentTicketCreator {
-    void create(ImpedimentTicket ticket);
+    ImpedimentTicket create(ImpedimentTicket ticket);
 }
