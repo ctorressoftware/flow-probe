8885a318487dd654872db2591c63ce0fd879d712
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-28 01:43:09 -0400

    feat: add create and restore methods to ImpedimentTicket domain model

--- src/main/java/io/github/ctorressoftware/domain/model/ImpedimentTicket.java
+++ src/main/java/io/github/ctorressoftware/domain/model/ImpedimentTicket.java
@@ -1,12 +1,26 @@
 package io.github.ctorressoftware.domain.model;
 
 public class ImpedimentTicket {
+    private final Long id;
     private final String title;
     private final String description;
 
-    public ImpedimentTicket(String title, String description) {
+    private ImpedimentTicket(Long id, String title, String description) {
+        this.id = id;
         this.title = title;
         this.description = description;
+    }
+
+    public static ImpedimentTicket create(String title, String description) {
+        return new ImpedimentTicket(null, title, description);
+    }
+
+    public static ImpedimentTicket restore(Long id, String title, String description) {
+        return new ImpedimentTicket(id, title, description);
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getDescription() {
