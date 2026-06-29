a3b6b1bd3de4624e009d26eaa6bab2fb5bcf4766
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-28 22:30:30 -0400

    feat: add RequestRenderer interface as out-port

--- src/main/java/io/github/ctorressoftware/application/port/out/RequestRenderer.java
+++ src/main/java/io/github/ctorressoftware/application/port/out/RequestRenderer.java
@@ -1,5 +1,9 @@
 package io.github.ctorressoftware.application.port.out;
 
-public interface ExecutableRequestRenderer {
+import io.github.ctorressoftware.domain.model.ReproducibleRequest;
+import io.github.ctorressoftware.domain.model.RequestFormat;
 
+public interface RequestRenderer {
+    String render(ReproducibleRequest request);
+    boolean supports(RequestFormat format);
 }
