1b36e82845215a9f049e89a48cc709d9600b7628
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-28 22:30:04 -0400

    feat: add RequestFormat enum

---
+++ src/main/java/io/github/ctorressoftware/domain/model/RequestFormat.java
@@ -0,0 +1,11 @@
+package io.github.ctorressoftware.domain.model;
+
+public enum RequestFormat {
+    CURL,
+    WGET,
+    JS_FETCH,
+    PYTHON_REQUEST,
+    POWERSHELL,
+    HURL,
+    HTTP_RAW
+}
