bdceb36c0cee19dec179b013338deed7b3d3d4e5
Carlos Torres <ctorressoftware@hotmail.com>
2026-06-29 23:32:42 -0400

    refactor: adjust line break

--- src/main/java/io/github/ctorressoftware/application/port/in/flowexecution/ExecuteFlowCommand.java
+++ src/main/java/io/github/ctorressoftware/application/port/in/flowexecution/ExecuteFlowCommand.java
@@ -2,5 +2,4 @@ package io.github.ctorressoftware.application.port.in.flowexecution;
 
 import io.github.ctorressoftware.domain.model.Flow;
 
-public record ExecuteFlowCommand(Flow flow) {
-}
+public record ExecuteFlowCommand(Flow flow) {}
