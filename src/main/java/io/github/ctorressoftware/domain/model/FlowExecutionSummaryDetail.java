fa0887e1ae3ebeee0da0a4db2579929884d89f33
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-03 17:22:24 -0400

    refactor: change FlowExecutionSummaryDetail to record

--- src/main/java/io/github/ctorressoftware/domain/model/FlowExecutionSummaryDetail.java
+++ src/main/java/io/github/ctorressoftware/domain/model/FlowExecutionSummaryDetail.java
@@ -1,33 +1,11 @@
 package io.github.ctorressoftware.domain.model;
 
-public class FlowExecutionSummaryDetail {
-    private String stepName;
-    private boolean isSuccessful;
-    private String responseString;
-
-    public FlowExecutionSummaryDetail() {}
-
-    public String getStepName() {
-        return stepName;
-    }
-
-    public void setStepName(String stepName) {
-        this.stepName = stepName;
-    }
-
-    public boolean isSuccessful() {
-        return isSuccessful;
-    }
-
-    public void setSuccessful(boolean successful) {
-        isSuccessful = successful;
-    }
-
-    public String getResponseString() {
-        return responseString;
-    }
-
-    public void setResponseString(String responseString) {
-        this.responseString = responseString;
-    }
-}
+import java.time.Duration;
+// TODO: Consider adding startedAt and finishedAt fields in the future
+public record FlowExecutionSummaryDetail(
+    String stepName,
+    boolean successful,
+    ServiceCall executed,
+    Duration executionDuration,
+    String rawResponse
+) {} // TODO: add validations
