8a05cacefd8c4e7f90380116acb47198933a5f68
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-02 22:43:12 -0400

    feat: add custom exception

---
+++ src/main/java/io/github/ctorressoftware/domain/exception/HttpServiceCallException.java
@@ -0,0 +1,11 @@
+package io.github.ctorressoftware.domain.exception;
+
+public class ServiceCallException extends RuntimeException {
+    public ServiceCallException(String message) {
+        super(message);
+    }
+
+    public ServiceCallException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
