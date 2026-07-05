ace24e19d7c2993ce05a51635b8ae5a505d0922a
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-05 18:39:49 -0400

    feat: add fromServiceCall static method to facilitate ReproducibleRequest object creation

--- src/main/java/io/github/ctorressoftware/domain/model/ReproducibleRequest.java
+++ src/main/java/io/github/ctorressoftware/domain/model/ReproducibleRequest.java
@@ -1,10 +1,25 @@
 package io.github.ctorressoftware.domain.model;
 
 import java.util.Map;
+import java.util.Objects;
 
 public record ReproducibleRequest(
         String url,
         String method,
         Map<String, String> headers,
         String body
-) {}
+) {
+    public static ReproducibleRequest fromServiceCall(ServiceCall serviceCall) {
+
+        if (serviceCall == null) {
+            throw new IllegalArgumentException("serviceCall cannot be null");
+        }
+
+        return new ReproducibleRequest(
+                serviceCall.url(),
+                serviceCall.method(),
+                serviceCall.headers(),
+                Objects.isNull(serviceCall.body()) ? null :  serviceCall.body().toString()
+        );
+    }
+}
