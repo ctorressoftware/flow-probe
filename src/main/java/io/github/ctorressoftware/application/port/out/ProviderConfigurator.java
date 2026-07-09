0448aebc155da707c51dea30323a139830ad678d
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-09 08:22:03 -0400

    feat: add parameters to configure definition method

--- src/main/java/io/github/ctorressoftware/application/port/out/ProviderConfigurator.java
+++ src/main/java/io/github/ctorressoftware/application/port/out/ProviderConfigurator.java
@@ -2,8 +2,10 @@ package io.github.ctorressoftware.application.port.out;
 
 import io.github.ctorressoftware.application.port.in.provider.configure.ProviderStatus;
 
+import java.util.Map;
+
 public interface ProviderConfigurator {
-    void configure();
+    void configure(Map<String, String> credentials);
 
     void remove();
 
