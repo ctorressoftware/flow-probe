343222d938991c2c7ac74b66ab12643cd39b0161
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-09 08:23:11 -0400

    refactor: add credentials parameter to configure implementation method

--- src/main/java/io/github/ctorressoftware/infrastructure/provider/azure/AzureProviderConfigurator.java
+++ src/main/java/io/github/ctorressoftware/infrastructure/provider/azure/AzureProviderConfigurator.java
@@ -3,34 +3,13 @@ package io.github.ctorressoftware.infrastructure.provider.azure;
 import io.github.ctorressoftware.application.port.in.provider.configure.ProviderStatus;
 import io.github.ctorressoftware.application.port.out.ProviderConfigurator;
 
-import java.util.Scanner;
+import java.util.Map;
 
 public class AzureProviderConfigurator implements ProviderConfigurator {
 
-    private final Scanner scanner;
-
-    public AzureProviderConfigurator(Scanner scanner) {
-        this.scanner = scanner;
-    }
-
     @Override
-    public void configure() {
-        System.out.print("Organization: ");
-        String azureOrganization = scanner.nextLine();
-        System.out.println();
+    public void configure(Map<String, String> credentials) {
 
-        System.out.print("Project: ");
-        String azureProject = scanner.nextLine();
-        System.out.println();
-
-        System.out.print("PAT: ");
-        String azurePAT = scanner.nextLine();
-        System.out.println();
-
-        System.out.println("---------");
-        System.out.println("Organization: " + azureOrganization);
-        System.out.println("Project: " + azureProject);
-        System.out.println("PAT: " + azurePAT);
     }
 
     @Override
