2280c27447a6a7ec511b0ff923f3b4224ce467ab
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-09 08:28:38 -0400

    feat: add KeystoreProviderConfigRepository implementation bootstrap

---
+++ src/main/java/io/github/ctorressoftware/infrastructure/persistence/repository/KeystoreProviderConfigRepository.java
@@ -0,0 +1,11 @@
+package io.github.ctorressoftware.infrastructure.persistence.repository;
+
+import io.github.ctorressoftware.application.port.out.ProviderConfigRepository;
+
+public class KeystoreProviderConfigRepository implements ProviderConfigRepository {
+
+    @Override
+    public void save() {
+
+    }
+}
