236756eb16a26262399ca780d9c8a0fe257c16fc
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-10 23:57:12 -0400

    refactor: rename ProviderCredentialsStorageManager to CredentialsStorageManager

--- src/main/java/io/github/ctorressoftware/application/port/out/CredentialsStorageManager.java
+++ src/main/java/io/github/ctorressoftware/application/port/out/CredentialsStorageManager.java
@@ -1,6 +1,6 @@
 package io.github.ctorressoftware.application.port.out;
 
-public interface ProviderCredentialsStorageManager {
+public interface CredentialsStorageManager {
     String find(String domain, String account);
     void store(String domain, String account, String secret);
     void delete(String domain, String account);
