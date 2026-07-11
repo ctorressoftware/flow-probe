016044ceaa1db80e8395373247439d1d0f04085c
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-11 00:01:03 -0400

    feat: implement providerConfigRepository in AzureProviderConfigurator

--- src/main/java/io/github/ctorressoftware/infrastructure/provider/azure/AzureProviderConfigurator.java
+++ src/main/java/io/github/ctorressoftware/infrastructure/provider/azure/AzureProviderConfigurator.java
@@ -1,24 +1,33 @@
 package io.github.ctorressoftware.infrastructure.provider.azure;
 
 import io.github.ctorressoftware.application.port.in.provider.configure.ProviderStatus;
+import io.github.ctorressoftware.application.port.out.ProviderConfigRepository;
 import io.github.ctorressoftware.application.port.out.ProviderConfigurator;
 
 import java.util.Map;
 
 public class AzureProviderConfigurator implements ProviderConfigurator {
 
+    private final ProviderConfigRepository providerConfigRepository;
+
+    public AzureProviderConfigurator(ProviderConfigRepository providerConfigRepository) {
+        this.providerConfigRepository = providerConfigRepository;
+    }
+
     @Override
     public void configure(Map<String, String> credentials) {
-
+        providerConfigRepository.save(credentials);
     }
 
     @Override
     public void remove() {
-
+        providerConfigRepository.remove();
     }
 
     @Override
     public ProviderStatus status() {
-        return null;
+        return providerConfigRepository.exists() ?
+                ProviderStatus.CONFIGURED :
+                ProviderStatus.NOT_CONFIGURED;
     }
 }
