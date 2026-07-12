738a64a0c7723c9888aad7c428a38bd0ff88fa9a
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-12 00:36:04 -0400

    feat: add AzureDevOpsConfiguration replacing AzureSettings previous configuration

--- src/main/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsWorkItemClient.java
+++ src/main/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsWorkItemClient.java
@@ -24,24 +24,25 @@ public class AzureDevOpsWorkItemClient {
         this.client = HttpClient.newHttpClient();
     }
 
-    public AzureDevOpsWorkItemResponse createWorkItem(AzureDevOpsCreateWorkItemRequest request) {
+    public AzureDevOpsWorkItemResponse createWorkItem(
+            AzureDevOpsCreateWorkItemRequest request,
+            AzureDevOpsConfiguration configuration) {
 
         ObjectMapper mapper = new ObjectMapper()
                 .registerModule(new JavaTimeModule())
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-        String endpoint = AzureSettings.AZURE_ORGANIZATION + "/" + AzureSettings.AZURE_PROJECT
-                + "/_apis/wit/workitems/$Issue?api-version=7.2-preview.3";
+        String endpoint = configuration.azureOrganization() + "/" + configuration.azureProject()
+                + "/_apis/wit/workitems/$" + configuration.azureWorkItemType() + "?api-version=7.2-preview.3";
 
         try {
-
-            URI uri = URI.create(AzureSettings.BASE_URL + "/" + endpoint);
-
+            URI uri = URI.create(AzureDevOpsConfiguration.AZURE_BASE_URL + endpoint);
             String jsonBody = mapper.writeValueAsString(request.operations());
 
             String base64Credentials = Base64
                     .getEncoder()
-                    .encodeToString((":" + AzureSettings.AZURE_PAT).getBytes(StandardCharsets.UTF_8));
+                    .encodeToString((":" + configuration.azurePat())
+                            .getBytes(StandardCharsets.UTF_8));
 
             HttpRequest httpRequest = HttpRequest
                     .newBuilder(uri)
@@ -50,8 +51,7 @@ public class AzureDevOpsWorkItemClient {
                     .header("Authorization", "Basic " + base64Credentials)
                     .build();
 
-            HttpResponse<String> response = client
-                    .send(httpRequest, BodyHandlers.ofString());
+            HttpResponse<String> response = client.send(httpRequest, BodyHandlers.ofString());
 
             if (response.statusCode() < HttpStatusCode.OK || response.statusCode() >= HttpStatusCode.MULTIPLE_CHOICES) {
                 throw new RuntimeException(
@@ -64,7 +64,7 @@ public class AzureDevOpsWorkItemClient {
 
         } catch (IOException | InterruptedException e) {
             Thread.currentThread().interrupt();
-            throw new RuntimeException("Error calling Azure DevOps API", e);
+            throw new RuntimeException("Error calling Azure DevOps API", e); // TODO: create a custom one
         }
     }
 }
