57e37f53864dfc7772ac44900b6eb749253a5d9a
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-12 02:16:02 -0400

    refactor: adjust YAMLReader to work in a native environment properly

--- src/main/java/io/github/ctorressoftware/infrastructure/readfile/YAMLReader.java
+++ src/main/java/io/github/ctorressoftware/infrastructure/readfile/YAMLReader.java
@@ -3,51 +3,63 @@ package io.github.ctorressoftware.infrastructure.readfile;
 import io.github.ctorressoftware.application.port.out.FlowFileReader;
 import io.github.ctorressoftware.domain.model.FilePath;
 import io.github.ctorressoftware.domain.model.Flow;
-import io.github.ctorressoftware.domain.model.ServiceCall;
 import io.github.ctorressoftware.domain.model.FlowStep;
+import io.github.ctorressoftware.domain.model.ServiceCall;
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class YAMLReader implements FlowFileReader {
 
+    @Override
     @SuppressWarnings("unchecked")
     public Flow read(FilePath filePath) {
 
         Yaml yaml = new Yaml();
-        InputStream inputStream = this.getClass()
-                .getClassLoader()
-                .getResourceAsStream(filePath.value());
 
-        Map<String, Object> yamlMap = (Map<String, Object>) yaml.load(inputStream);
+        try (InputStream inputStream = Files.newInputStream(Path.of(filePath.value()))) {
 
-        String flowName = yamlMap.get("name").toString();
+            Map<String, Object> yamlMap = (Map<String, Object>) yaml.load(inputStream);
 
-        List<Map<String, Object>> steps = (List<Map<String, Object>>) yamlMap.get("steps");
+            String flowName = yamlMap.get("name").toString();
 
-        List<FlowStep> formattedSteps = new ArrayList<>();
+            List<Map<String, Object>> steps =
+                    (List<Map<String, Object>>) yamlMap.get("steps");
 
-        for (Map<String, Object> step : steps) {
-            String stepName = step.get("name").toString();
+            List<FlowStep> formattedSteps = new ArrayList<>();
 
-            Map<String, Object> request = (Map<String, Object>) step.get("request");
-            Map<String, Object> requires = (Map<String, Object>) step.get("requires");
-            Map<String, String> export = (Map<String, String>) step.get("export");
+            for (Map<String, Object> step : steps) {
 
-            String url = request.get("url").toString();
-            String method = request.get("method").toString();
-            Map<String, String> headers = (Map<String, String>) request.get("headers");
-            Object body = request.get("body");
+                String stepName = step.get("name").toString();
 
-            ServiceCall serviceCall = resolveRequest(url, method, headers, body);
-            FlowStep flowStep = resolveStep(flowName, stepName, serviceCall, requires, export);
-            formattedSteps.add(flowStep);
+                Map<String, Object> request = (Map<String, Object>) step.get("request");
+                Map<String, Object> requires = (Map<String, Object>) step.get("requires");
+                Map<String, String> export = (Map<String, String>) step.get("export");
+
+                String url = request.get("url").toString();
+                String method = request.get("method").toString();
+
+                Map<String, String> headers =
+                        (Map<String, String>) request.get("headers");
+
+                Object body = request.get("body");
+
+                ServiceCall serviceCall = resolveRequest(url, method, headers, body);
+                FlowStep flowStep = resolveStep(flowName, stepName, serviceCall, requires, export);
+
+                formattedSteps.add(flowStep);
+            }
+
+            return Flow.create(flowName, formattedSteps);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read YAML file: " + filePath.value(), e); // TODO: create custom one
         }
-
-        return Flow.create(flowName, formattedSteps);
     }
 
     private ServiceCall resolveRequest(
