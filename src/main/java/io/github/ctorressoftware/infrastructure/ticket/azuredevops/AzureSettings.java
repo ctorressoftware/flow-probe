41176e4a68d0aadf408b52222f1c2717125e131b
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-12 00:29:39 -0400

    chore: remove AzureSettings temporary file

--- src/main/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureSettings.java
+++
@@ -1,9 +0,0 @@
-package io.github.ctorressoftware.infrastructure.ticket.azuredevops;
-
-/* TODO: Temporary file. Remove it later */
-public class AzureSettings {
-    public static String BASE_URL = "https://dev.azure.com";
-    public static String AZURE_ORGANIZATION = "your-organization";
-    public static String AZURE_PROJECT = "your-project";
-    public static String AZURE_PAT = "your-pat";
-}
