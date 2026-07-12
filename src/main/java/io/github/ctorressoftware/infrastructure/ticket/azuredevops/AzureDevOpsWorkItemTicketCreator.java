889b8f8efe45051f16fedd20a147369191cd1af4
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-12 00:30:51 -0400

    feat: add ProviderConfigRepository dependency to get required azure configuration data

--- src/main/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsWorkItemTicketCreator.java
+++ src/main/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsWorkItemTicketCreator.java
@@ -1,17 +1,44 @@
 package io.github.ctorressoftware.infrastructure.ticket.azuredevops;
 
+import io.github.ctorressoftware.application.port.out.ProviderConfigRepository;
 import io.github.ctorressoftware.domain.model.ImpedimentTicket;
+
+import java.util.Map;
 
 public class AzureDevOpsWorkItemTicketCreator {
 
     private final AzureDevOpsWorkItemClient azureDevOpsWorkItemClient;
+    private final ProviderConfigRepository providerConfigRepository;
 
-    public AzureDevOpsWorkItemTicketCreator(AzureDevOpsWorkItemClient azureDevOpsWorkItemClient) {
+    public AzureDevOpsWorkItemTicketCreator(
+            AzureDevOpsWorkItemClient azureDevOpsWorkItemClient,
+            ProviderConfigRepository providerConfigRepository) {
         this.azureDevOpsWorkItemClient = azureDevOpsWorkItemClient;
+        this.providerConfigRepository = providerConfigRepository;
     }
 
     public AzureDevOpsWorkItemResponse create(ImpedimentTicket ticket) {
+        var configuration = getAzureConfiguration();
         var request = AzureDevOpsCreateWorkItemRequest.from(ticket);
-        return azureDevOpsWorkItemClient.createWorkItem(request);
+        return azureDevOpsWorkItemClient.createWorkItem(request, configuration);
+    }
+
+    private AzureDevOpsConfiguration getAzureConfiguration() {
+
+        Map<String, String> azureCredentials = providerConfigRepository.findByDomainAndAccount(
+                AzureDevOpsConfiguration.AZURE_DOMAIN,
+                AzureDevOpsConfiguration.AZURE_ACCOUNT
+        );
+
+        String azureOrganization = azureCredentials.get("organization");
+        String azureProject = azureCredentials.get("project");
+        String azurePat = azureCredentials.get("pat");
+
+        return new AzureDevOpsConfiguration(
+                "Issue",
+                azureOrganization,
+                azureProject,
+                azurePat
+        );
     }
 }
