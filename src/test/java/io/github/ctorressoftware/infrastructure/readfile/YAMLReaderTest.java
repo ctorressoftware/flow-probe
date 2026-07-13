415b6c2e9319794e1e6fd34a4090e40695e8bda5
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-12 23:37:08 -0400

    test: add YAMLReaderTest and valid-full.yaml file

---
+++ src/test/java/io/github/ctorressoftware/infrastructure/readfile/YAMLReaderTest.java
@@ -0,0 +1,21 @@
+package io.github.ctorressoftware.infrastructure.readfile;
+
+import io.github.ctorressoftware.domain.model.FilePath;
+import io.github.ctorressoftware.domain.model.Flow;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class YAMLReaderTest {
+    private static final String BASE_PATH = "src/test/resources/yaml-cases/";
+
+    @Test
+    void parsesValidFullFlow() {
+        YAMLReader reader = new YAMLReader();
+
+        Flow flow = reader.read(new FilePath(BASE_PATH + "valid-full.yaml"));
+
+        assertEquals("pokeapi-success-flow", flow.getName());
+        assertEquals(3, flow.getSteps().size());
+    }
+}
