010d41889353a6ebaf2ec07749a426f8fd55b0a9
Carlos Torres <ctorressoftware@hotmail.com>
2026-07-27 00:47:04 -0400

    refactor:

--- src/main/java/io/github/ctorressoftware/AppConfig.java
+++ src/main/java/io/github/ctorressoftware/AppConfig.java
@@ -1,5 +1,6 @@
 package io.github.ctorressoftware;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.ctorressoftware.application.port.in.createticket.CreateImpedimentTicketUseCase;
 import io.github.ctorressoftware.application.port.in.flowexecution.ExecuteFlowUseCase;
 import io.github.ctorressoftware.application.port.in.provider.configure.ConfigureProviderUseCase;
@@ -12,6 +13,7 @@ import io.github.ctorressoftware.application.usecase.flowexecution.ExecuteFlowHa
 import io.github.ctorressoftware.application.usecase.flowexecution.FlowExecutor;
 import io.github.ctorressoftware.application.usecase.provider.configure.ConfigureProviderHandler;
 import io.github.ctorressoftware.domain.model.Context;
+import io.github.ctorressoftware.infrastructure.callservice.RequestMapper;
 import io.github.ctorressoftware.infrastructure.callservice.RestServiceCaller;
 import io.github.ctorressoftware.infrastructure.persistence.adapter.KeystoreProviderConfigRepositoryAdapter;
 import io.github.ctorressoftware.infrastructure.persistence.keystore.KeystoreCredentialsStorageManager;
@@ -23,21 +25,25 @@ import io.github.ctorressoftware.infrastructure.ticket.azuredevops.AzureDevOpsIm
 import io.github.ctorressoftware.infrastructure.ticket.azuredevops.AzureDevOpsWorkItemClient;
 import io.github.ctorressoftware.infrastructure.ticket.azuredevops.AzureDevOpsWorkItemTicketCreator;
 
+import java.net.http.HttpClient;
 import java.util.Map;
 import java.util.Scanner;
 
 public final class AppConfig {
-    private final RequestRenderer requestRenderer = new CurlRequestRenderer();
+    private final Context context = new Context();
     private final Scanner scanner = new Scanner(System.in);
+    private final HttpClient httpClient = HttpClient.newHttpClient();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final RequestMapper requestMapper = new RequestMapper(objectMapper);
+    private final RequestRenderer requestRenderer = new CurlRequestRenderer(objectMapper);
     private final FlowFileReader flowFileReader = new YamlReader();
     private final ReadFileUseCase readFileUseCase = new ReadFileHandler(flowFileReader);
-    private final Context context = new Context();
-    private final ServiceCaller serviceCaller = new RestServiceCaller();
-    private final FlowExecutor flowExecutor = new FlowExecutor(context, serviceCaller);
+    private final ServiceCaller serviceCaller = new RestServiceCaller(httpClient, requestMapper);
+    private final FlowExecutor flowExecutor = new FlowExecutor(context, serviceCaller, objectMapper);
     private final ExecuteFlowUseCase executeFlowUseCase = new ExecuteFlowHandler(flowExecutor);
     private final AzureDevOpsWorkItemClient azureDevOpsWorkItemClient = new AzureDevOpsWorkItemClient();
     private final CredentialsStorageManager credentialsStorageManager = new KeystoreCredentialsStorageManager();
-    private final ProviderConfigRepository providerConfigRepository = new KeystoreProviderConfigRepositoryAdapter(credentialsStorageManager);
+    private final ProviderConfigRepository providerConfigRepository = new KeystoreProviderConfigRepositoryAdapter(objectMapper, credentialsStorageManager);
     private final AzureDevOpsWorkItemTicketCreator azureDevOpsWorkItemTicketCreator = new AzureDevOpsWorkItemTicketCreator(azureDevOpsWorkItemClient, providerConfigRepository);
     private final ImpedimentTicketCreator impedimentTicketCreator = new AzureDevOpsImpedimentTicketCreatorAdapter(azureDevOpsWorkItemTicketCreator);
     private final CreateImpedimentTicketUseCase createImpedimentTicketUseCase = new CreateImpedimentTicketHandler(impedimentTicketCreator);
