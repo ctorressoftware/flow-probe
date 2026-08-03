6a081526d716b32ad3728dea5b0c5d00f2f98f3a
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-02 21:48:48 -0400

    test: add shouldReadPatFromConsoleWhenConsoleIsAvailable test and refactor testAzurePromptData

--- src/test/java/io/github/ctorressoftware/infrastructure/provider/azure/AzureProviderPromptTest.java
+++ src/test/java/io/github/ctorressoftware/infrastructure/provider/azure/AzureProviderPromptTest.java
@@ -1,13 +1,13 @@
 package io.github.ctorressoftware.infrastructure.provider.azure;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.Console;
 import java.io.PrintStream;
 import java.util.Map;
 import java.util.Scanner;
@@ -21,15 +21,18 @@ class AzureProviderPromptTest {
     @Mock
     private Scanner scanner;
 
+    @Mock
+    private Console console;
+
+    private static final String PAT_PROMPT =
+            "Write your Azure DevOps Personal Access Token (PAT): ";
+
     private AzureProviderPrompt azureProviderPrompt;
 
-    @BeforeEach
-    void init() {
-        azureProviderPrompt = new AzureProviderPrompt(out, scanner);
-    }
-
     @Test
-    void testAzurePromptData() {
+    void shouldReadAllValuesFromScannerWhenConsoleIsUnavailable() {
+
+        azureProviderPrompt = new AzureProviderPrompt(out, scanner, null);
 
         Map<String, String> azureTestData = Map.of(
                 "organization", "test",
@@ -43,6 +46,35 @@ class AzureProviderPromptTest {
         Map<String, String> data = azureProviderPrompt.prompt();
 
         Assertions.assertEquals(azureTestData, data);
+
+        Mockito.verify(scanner, Mockito.times(4)).nextLine();
+        Mockito.verifyNoInteractions(console);
     }
 
+    @Test
+    void shouldReadPatFromConsoleWhenConsoleIsAvailable() {
+
+        azureProviderPrompt = new AzureProviderPrompt(out, scanner, console);
+
+        Map<String, String> azureTestData = Map.of(
+                "organization", "test",
+                "project", "test",
+                "workItemType", "test",
+                "pat", "test"
+        );
+
+        Mockito.when(scanner.nextLine()).thenReturn("test");
+
+        Mockito
+                .when(console.readPassword(PAT_PROMPT))
+                .thenReturn("test".toCharArray());
+
+        Map<String, String> data = azureProviderPrompt.prompt();
+
+        Assertions.assertEquals(azureTestData, data);
+
+        Mockito.verify(scanner, Mockito.times(3)).nextLine();
+        Mockito.verify(console, Mockito.times(1)).readPassword(PAT_PROMPT);
+        Mockito.verifyNoMoreInteractions(scanner, console);
+    }
 }
