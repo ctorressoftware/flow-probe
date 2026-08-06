843dda591020d3680fda3fad9cbc694672d41bab
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-05 23:23:48 -0400

    test: refactor shouldExecuteFlowWithStepsCorrectly to shouldExecuteFlowWithMultipleStepsWithoutExportingVariables with better logic

--- src/test/java/io/github/ctorressoftware/application/usecase/flowexecution/FlowExecutorTest.java
+++ src/test/java/io/github/ctorressoftware/application/usecase/flowexecution/FlowExecutorTest.java
@@ -3,6 +3,7 @@ package io.github.ctorressoftware.application.usecase.flowexecution;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.ctorressoftware.application.port.out.ServiceCaller;
 import io.github.ctorressoftware.domain.constant.HttpMethod;
+import io.github.ctorressoftware.domain.constant.HttpStatusCode;
 import io.github.ctorressoftware.domain.model.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -36,33 +37,31 @@ class FlowExecutorTest {
     }
 
     @Test
-    void shouldExecuteFlowWithStepsCorrectly() {
+    void shouldExecuteFlowWithMultipleStepsWithoutExportingVariables() {
 
-        ServiceCall call = new ServiceCall(
+        ServiceCall serviceCall = new ServiceCall(
                 "https://pokeapi.co/api/v2/pokemon?offset=0&limit=1350",
                 HttpMethod.GET,
                 Map.of("accept", "application/json"),
                 null
         );
 
-        FlowStep step = FlowStep.create(
-                "flow",
-                "first_step",
-                call,
-                null,
-                null
+        List<FlowStep> steps = List.of(
+                FlowStep.create("flow", "first", serviceCall, null, null),
+                FlowStep.create("flow", "second", serviceCall, null, null),
+                FlowStep.create("flow", "third", serviceCall, null, null)
         );
 
-        Mockito.when(serviceCaller.call(call)).thenReturn(
-                new CallResult(200, null)
-        );
+        CallResult result = new CallResult(HttpStatusCode.OK, null);
 
-        FlowExecutionSummary summary = flowExecutor
-                .execute(Flow.create("flow", List.of(step)));
+        Mockito.when(serviceCaller.call(serviceCall)).thenReturn(result);
+
+        FlowExecutionSummary summary = flowExecutor.execute(Flow.create("flow", steps));
 
         Assertions.assertNotNull(summary);
-        Assertions.assertEquals(summary.getFlowName(), step.getFlowName());
+        Assertions.assertEquals(summary.getFlowName(), steps.getFirst().getFlowName());
         Assertions.assertTrue(summary.isSuccessfulExecution());
-        Assertions.assertEquals(1, summary.getStepsResults().size());
+        Assertions.assertEquals(3, summary.getStepsResults().size());
+        Mockito.verify(serviceCaller, Mockito.times(3)).call(serviceCall);
     }
 }
