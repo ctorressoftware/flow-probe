2b1e70275adebb6b75b64c686f1074a32b944c3c
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-10 00:57:41 -0400

    refactor: improve PlaceholderResolver logic and make methods non-static

--- src/main/java/io/github/ctorressoftware/application/usecase/flowexecution/PlaceholderResolver.java
+++ src/main/java/io/github/ctorressoftware/application/usecase/flowexecution/PlaceholderResolver.java
@@ -1,12 +1,47 @@
 package io.github.ctorressoftware.application.usecase.flowexecution;
 
 import io.github.ctorressoftware.domain.model.ContextVariable;
+import io.github.ctorressoftware.domain.model.ServiceCall;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PlaceholderResolver {
 
-    public static String resolve(List<ContextVariable> variables, String value) {
+    private final JsonUtils jsonUtils;
+
+    public PlaceholderResolver(JsonUtils jsonUtils) {
+        this.jsonUtils = jsonUtils;
+    }
+
+    public ServiceCall resolve(List<ContextVariable> variables, ServiceCall serviceCall) {
+
+        if (serviceCall == null) {
+            throw new IllegalArgumentException("serviceCall cannot be null");
+        }
+
+        return new ServiceCall(
+                resolveString(variables, serviceCall.url()),
+                resolveString(variables, serviceCall.method()),
+                resolveMap(variables, serviceCall.headers()),
+                resolveBody(variables, serviceCall.body())
+        );
+    }
+
+    private Object resolveBody(
+            List<ContextVariable> variables,
+            Object body
+    ) {
+        if (body == null) {
+            return null;
+        }
+
+        return resolveString(variables, jsonUtils.serialize(body));
+    }
+
+    public String resolveString(List<ContextVariable> variables, String value) {
+
         if (value == null) return null;
 
         String resolved = value;
@@ -16,5 +51,29 @@ public class PlaceholderResolver {
         }
 
         return resolved;
+    }
+
+    public Map<String, String> resolveMap(List<ContextVariable> variables, Map<String, String> stringMap) {
+        if (stringMap == null) return null;
+
+        Map<String, String> mapResolved = new HashMap<>();
+
+        for (Map.Entry<String, String> entry : stringMap.entrySet()) {
+            String newKey = entry.getKey();
+            String newVal = entry.getValue();
+
+            for (ContextVariable variable : variables) {
+                String placeholder = "${" + variable.name() + "}";
+                String replacement = variable.value() != null ? variable.value().toString() : "";
+
+                newKey = newKey.replace(placeholder, replacement);
+                if (newVal != null) {
+                    newVal = newVal.replace(placeholder, replacement);
+                }
+            }
+            mapResolved.put(newKey, newVal);
+        }
+
+        return mapResolved;
     }
 }
