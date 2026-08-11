2efb49e6dfb516535218926188e0ec1445a19231
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-10 23:38:43 -0400

    refactor: remove ContextManager outport

--- src/main/java/io/github/ctorressoftware/application/usecase/flowexecution/ContextManager.java
+++ src/main/java/io/github/ctorressoftware/application/usecase/flowexecution/ContextManager.java
@@ -1,6 +1,5 @@
 package io.github.ctorressoftware.application.usecase.flowexecution;
 
-import io.github.ctorressoftware.application.port.out.ContextManager;
 import io.github.ctorressoftware.application.port.out.JsonProcessor;
 import io.github.ctorressoftware.domain.model.Context;
 import io.github.ctorressoftware.domain.model.ContextVariable;
@@ -9,17 +8,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-public class FlowContextManager implements ContextManager {
+public class ContextManager {
 
     private final Context context;
     private final JsonProcessor jsonProcessor;
 
-    public FlowContextManager(Context context, JsonProcessor jsonProcessor) {
+    public ContextManager(Context context, JsonProcessor jsonProcessor) {
         this.context = Objects.requireNonNull(context);
         this.jsonProcessor = Objects.requireNonNull(jsonProcessor);
     }
 
-    @Override
     public void exportVariables(String data, Map<String, String> toExport) {
 
         if (toExport == null || toExport.isEmpty()) {
@@ -31,7 +29,6 @@ public class FlowContextManager implements ContextManager {
         );
     }
 
-    @Override
     public List<ContextVariable> getVariables() {
         return context.variables();
     }
