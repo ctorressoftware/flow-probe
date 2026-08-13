18ea9e99002c0abc1e9086d2341f9a710ceb9ae4
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-12 23:51:40 -0400

    test: add JacksonJsonProcessorTest file

---
+++ src/test/java/io/github/ctorressoftware/infrastructure/json/jackson/JacksonJsonProcessorTest.java
@@ -0,0 +1,8 @@
+package io.github.ctorressoftware.infrastructure.json.jackson;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class JacksonJsonProcessorTest {
+}
