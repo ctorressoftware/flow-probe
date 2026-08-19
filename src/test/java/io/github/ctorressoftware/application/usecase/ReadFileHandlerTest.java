ea8f045811b49241468c0a78fbe2426e0c469c96
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-19 00:53:52 -0400

    test: add shouldReadSpecifiedFile to ReadFileHandlerTest

--- src/test/java/io/github/ctorressoftware/application/usecase/ReadFileHandlerTest.java
+++ src/test/java/io/github/ctorressoftware/application/usecase/ReadFileHandlerTest.java
@@ -1,5 +1,59 @@
 package io.github.ctorressoftware.application.usecase;
 
+import io.github.ctorressoftware.application.port.in.readfile.ReadFileCommand;
+import io.github.ctorressoftware.application.port.in.readfile.ReadFileResult;
+import io.github.ctorressoftware.application.port.out.FlowFileReader;
+import io.github.ctorressoftware.domain.model.FilePath;
+import io.github.ctorressoftware.domain.model.Flow;
+import io.github.ctorressoftware.domain.model.FlowStep;
+import io.github.ctorressoftware.domain.model.ServiceCall;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 public class ReadFileHandlerTest {
-    // TODO: create tests
+
+    @Mock
+    private FlowFileReader reader;
+
+    private ReadFileHandler handler;
+
+    @BeforeEach
+    void init() {
+        handler = new ReadFileHandler(reader);
+    }
+
+    @Test
+    void shouldReadSpecifiedFile() {
+
+        FilePath filePath = new FilePath("flow.yaml");
+        ReadFileCommand command = new ReadFileCommand(filePath);
+
+        ServiceCall serviceCall = new ServiceCall(
+                "https://example.com",
+                "GET",
+                null,
+                null
+        );
+
+        Flow flow = Flow.create(
+                "test-flow",
+                List.of(FlowStep.create("flow", "step", serviceCall, null, null))
+        );
+
+        Mockito.when(reader.read(filePath))
+                .thenReturn(flow);
+
+        ReadFileResult result = handler.read(command);
+
+        Assertions.assertSame(flow, result.flow());
+        Mockito.verify(reader).read(filePath);
+    }
 }
