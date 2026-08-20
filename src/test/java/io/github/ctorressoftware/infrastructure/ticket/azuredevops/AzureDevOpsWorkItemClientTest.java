664988109f6f4d589fb645cbe6fef7dc08ea3ac3
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-20 07:42:43 -0400

    tests: add shouldCreateAzureDevOpsWorkItem test

--- src/test/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsWorkItemClientTest.java
+++ src/test/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsWorkItemClientTest.java
@@ -1,5 +1,65 @@
 package io.github.ctorressoftware.infrastructure.ticket.azuredevops;
 
+import io.github.ctorressoftware.domain.constant.HttpStatusCode;
+import io.github.ctorressoftware.domain.model.ImpedimentTicket;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@ExtendWith(MockitoExtension.class)
 public class AzureDevOpsWorkItemClientTest {
-    // TODO: create tests
+
+    @Mock
+    private HttpClient httpClient;
+
+    private AzureDevOpsWorkItemClient azureDevOpsClient;
+
+    @BeforeEach
+    void init() {
+        this.azureDevOpsClient = new AzureDevOpsWorkItemClient(httpClient);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked") // TODO: add ArgumentCaptor
+    void shouldCreateAzureDevOpsWorkItem() throws IOException, InterruptedException {
+
+        ImpedimentTicket ticket = ImpedimentTicket.create("Title", "Description");
+
+        AzureDevOpsCreateWorkItemRequest request = AzureDevOpsCreateWorkItemRequest.from(ticket);
+
+        AzureDevOpsConfiguration configuration = new AzureDevOpsConfiguration(
+                "Impediment",
+                "Organization",
+                "Project",
+                "1234567890987654321"
+        );
+
+        HttpResponse<String> mockResponse = Mockito.mock();
+
+        String mockJson = "{\"id\": 123, \"fields\": {\"System.Title\": \"Title\"}}";
+        Mockito.when(mockResponse.statusCode()).thenReturn(HttpStatusCode.OK);
+        Mockito.when(mockResponse.body()).thenReturn(mockJson);
+
+        Mockito.doReturn(mockResponse)
+                .when(httpClient)
+                .send(Mockito.any(), Mockito.any(HttpResponse.BodyHandler.class));
+
+        AzureDevOpsWorkItemResponse response = azureDevOpsClient
+                .createWorkItem(request, configuration);
+
+        Assertions.assertNotNull(response, "La respuesta no debería ser nula");
+        Assertions.assertEquals(123, response.id());
+        Mockito.verify(httpClient, Mockito.times(1))
+                .send(Mockito.any(HttpRequest.class), Mockito.any(HttpResponse.BodyHandler.class));
+
+    }
 }
