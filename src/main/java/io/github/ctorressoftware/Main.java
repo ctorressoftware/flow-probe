1df5cdd3cfd50046e0bd012a65166cb7e5741217
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-24 07:56:37 -0400

    feat: cutomize no-parameter exception

--- src/main/java/io/github/ctorressoftware/Main.java
+++ src/main/java/io/github/ctorressoftware/Main.java
@@ -1,6 +1,7 @@
 package io.github.ctorressoftware;
 
 import io.github.ctorressoftware.infrastructure.cli.ConfigureCommand;
+import io.github.ctorressoftware.infrastructure.cli.ExitCode;
 import io.github.ctorressoftware.infrastructure.cli.FlowProbeCommand;
 import io.github.ctorressoftware.infrastructure.cli.RunCommand;
 import picocli.CommandLine;
@@ -9,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         AppConfig config = new AppConfig();
         FlowProbeCommand rootCommand = new FlowProbeCommand();
-        CommandLine commandLine = new CommandLine(rootCommand);
+        CommandLine commandLine = commandLine(rootCommand);
 
         commandLine.addSubcommand("run", new RunCommand(
                 config.out(),
@@ -26,5 +27,16 @@ public class Main {
 
         int exitCode = commandLine.execute(args);
         System.exit(exitCode);
+    }
+
+    private static CommandLine commandLine(FlowProbeCommand rootCommand) {
+        CommandLine commandLine = new CommandLine(rootCommand);
+
+        commandLine.setParameterExceptionHandler((exception, varargs) -> {
+            CommandLine cmd = exception.getCommandLine();
+            cmd.getErr().println("Invalid command arguments: " + exception.getMessage());
+            return ExitCode.INVALID_ARGUMENTS.code();
+        });
+        return commandLine;
     }
 }
