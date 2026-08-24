f8532bff74715e2bdfd94eab9f0cd274ba97cf30
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-23 20:21:42 -0400

    chore: add ExitCode enum

---
+++ src/main/java/io/github/ctorressoftware/infrastructure/cli/ExitCode.java
@@ -0,0 +1,17 @@
+package io.github.ctorressoftware.infrastructure.cli;
+
+public enum ExitCode {
+    SUCCESS(0),
+    EXECUTION_ERROR(1),
+    INVALID_ARGUMENTS(2);
+
+    private final int code;
+
+    ExitCode(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
