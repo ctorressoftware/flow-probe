31a4668c07f3b92e0c478d2288c988b083785aad
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-24 23:05:40 -0400

    test: add shouldPrintUsageWhenExecutedWithoutSubcommand in FlowProbeCommandTest file

--- src/test/java/io/github/ctorressoftware/infrastructure/cli/FlowProbeCommandTest.java
+++ src/test/java/io/github/ctorressoftware/infrastructure/cli/FlowProbeCommandTest.java
@@ -1,5 +1,39 @@
 package io.github.ctorressoftware.infrastructure.cli;
 
-public class FlowProbeCommandTest {
-    // TODO: create tests
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+class FlowProbeCommandTest {
+
+    private final PrintStream originalOut = System.out;
+    private ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    void setUp() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void shouldPrintUsageWhenExecutedWithoutSubcommand() {
+
+        FlowProbeCommand command = new FlowProbeCommand();
+
+        command.run();
+        String output = outputStream.toString();
+
+        Assertions.assertTrue(output.contains("Usage:"));
+        Assertions.assertTrue(output.contains("flowprobe"));
+        Assertions.assertTrue(output.contains("CLI tool to execute and verify HTTP flows"));
+    }
 }
