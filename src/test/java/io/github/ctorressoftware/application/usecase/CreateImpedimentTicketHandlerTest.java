8fff27a77ee4baf669eb1e5d0a6f4523111b3b59
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-25 21:56:23 -0400

    test: add CreateImpedimentTicketHandlerTest boostrap

--- src/test/java/io/github/ctorressoftware/application/usecase/CreateImpedimentTicketHandlerTest.java
+++ src/test/java/io/github/ctorressoftware/application/usecase/CreateImpedimentTicketHandlerTest.java
@@ -1,5 +1,28 @@
 package io.github.ctorressoftware.application.usecase;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import io.github.ctorressoftware.application.port.out.ImpedimentTicketCreator;
+
+@ExtendWith(MockitoExtension.class)
 public class CreateImpedimentTicketHandlerTest {
-    // TODO: create tests
+    
+    @Mock
+    private ImpedimentTicketCreator impedimentTicketCreator;
+
+    private CreateImpedimentTicketHandler createImpedimentTicketHandler;
+
+    @BeforeEach
+    void init() {
+        this.createImpedimentTicketHandler = new CreateImpedimentTicketHandler(impedimentTicketCreator);
+    }
+
+    @Test
+    void shouldCreateTicketAndReturnResult() {
+        // TODO
+    }
 }
