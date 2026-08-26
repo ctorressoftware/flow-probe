979eaa161c8d860adae91c59a508991ee5e9b7c6
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-25 22:39:20 -0400

    test: add shouldCreateTicketAndReturnResult test to CreateImpedimentTicketHandlerTest

--- src/test/java/io/github/ctorressoftware/application/usecase/CreateImpedimentTicketHandlerTest.java
+++ src/test/java/io/github/ctorressoftware/application/usecase/CreateImpedimentTicketHandlerTest.java
@@ -1,9 +1,14 @@
 package io.github.ctorressoftware.application.usecase;
 
+import io.github.ctorressoftware.application.port.in.createticket.CreateImpedimentTicketCommand;
+import io.github.ctorressoftware.application.port.in.createticket.CreateImpedimentTicketResult;
+import io.github.ctorressoftware.domain.model.ImpedimentTicket;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import io.github.ctorressoftware.application.port.out.ImpedimentTicketCreator;
@@ -23,6 +28,30 @@ public class CreateImpedimentTicketHandlerTest {
 
     @Test
     void shouldCreateTicketAndReturnResult() {
-        // TODO
+
+        ImpedimentTicket ticket = ImpedimentTicket.create(
+                "Title",
+                "Description"
+        );
+
+        CreateImpedimentTicketCommand command = new CreateImpedimentTicketCommand(ticket);
+
+        ImpedimentTicket expected = ImpedimentTicket.restore(
+                123L,
+                ticket.title(),
+                ticket.description());
+
+        Mockito
+                .when(impedimentTicketCreator.create(ticket))
+                .thenReturn(ImpedimentTicket.restore(
+                        123L,
+                        ticket.title(),
+                        ticket.description()
+                ));
+
+        CreateImpedimentTicketResult result =
+                createImpedimentTicketHandler.createTicket(command);
+
+        Assertions.assertEquals(expected, result.created());
     }
 }
