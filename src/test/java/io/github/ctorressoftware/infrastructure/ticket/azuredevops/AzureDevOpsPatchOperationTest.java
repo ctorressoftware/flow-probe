185e0557f1a096c1c07644a8554ea2e29a25458e
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-26 10:55:18 -0400

    test: add AzureDevOpsPatchOperationTest file

--- src/test/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsPatchOperationTest.java
+++ src/test/java/io/github/ctorressoftware/infrastructure/ticket/azuredevops/AzureDevOpsPatchOperationTest.java
@@ -1,5 +1,24 @@
 package io.github.ctorressoftware.infrastructure.ticket.azuredevops;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AzureDevOpsPatchOperationTest {
-    // TODO: create tests
+
+    @Test
+    void shouldAddAzureDevOpsPatchOperationData() {
+
+        Object content = new Object();
+
+        AzureDevOpsPatchOperation patchOperation = 
+            AzureDevOpsPatchOperation.add("/azuredevops/path/", content);
+
+        AzureDevOpsPatchOperation expected = new AzureDevOpsPatchOperation(
+            "add", 
+            "/azuredevops/path/", 
+            content
+        );
+
+        Assertions.assertEquals(expected, patchOperation);
+    }
 }
