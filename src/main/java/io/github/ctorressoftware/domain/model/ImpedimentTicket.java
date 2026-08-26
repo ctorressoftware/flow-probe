a5e22a55beb9988df8cdd0777e7d763c2e650401
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-25 22:35:13 -0400

    refactor: convert ImpedimentTicket to a record

--- src/main/java/io/github/ctorressoftware/domain/model/ImpedimentTicket.java
+++ src/main/java/io/github/ctorressoftware/domain/model/ImpedimentTicket.java
@@ -1,15 +1,6 @@
 package io.github.ctorressoftware.domain.model;
 
-public class ImpedimentTicket {
-    private final Long id;
-    private final String title;
-    private final String description;
-
-    private ImpedimentTicket(Long id, String title, String description) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-    }
+public record ImpedimentTicket(Long id, String title, String description) {
 
     public static ImpedimentTicket create(String title, String description) {
         return new ImpedimentTicket(null, title, description);
@@ -17,17 +8,5 @@ public class ImpedimentTicket {
 
     public static ImpedimentTicket restore(Long id, String title, String description) {
         return new ImpedimentTicket(id, title, description);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getTitle() {
-        return title;
     }
 }
