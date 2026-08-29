8abad10edcd467eab6c6652ff1e2db50f3e8aba2
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-29 17:17:26 -0400

    chore: remove unused import

--- src/test/java/io/github/ctorressoftware/infrastructure/persistence/adapter/KeystoreProviderConfigRepositoryAdapterTest.java
+++ src/test/java/io/github/ctorressoftware/infrastructure/persistence/adapter/KeystoreProviderConfigRepositoryAdapterTest.java
@@ -14,7 +14,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
