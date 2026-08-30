4c2c468f50f818aeeb78fbfde97d2c8ce0cc9333
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-30 19:30:08 -0400

    refactor: convert FilePath class to a record

--- src/main/java/io/github/ctorressoftware/domain/model/FilePath.java
+++ src/main/java/io/github/ctorressoftware/domain/model/FilePath.java
@@ -7,11 +7,4 @@ public record FilePath(String value) {
     public FilePath(String value) {
         this.value = Objects.requireNonNull(value);
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        FilePath filePath = (FilePath) o;
-        return Objects.equals(value, filePath.value);
-    }
 }
