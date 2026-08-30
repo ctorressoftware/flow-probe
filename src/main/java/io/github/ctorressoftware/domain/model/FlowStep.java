f10d6eb1fd3a57ecdf405b7b8f71c60e27784046
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-30 19:50:27 -0400

    refactor: convert FlowStep class to a record

--- src/main/java/io/github/ctorressoftware/domain/model/FlowStep.java
+++ src/main/java/io/github/ctorressoftware/domain/model/FlowStep.java
@@ -1,34 +1,15 @@
 package io.github.ctorressoftware.domain.model;
 
-import io.github.ctorressoftware.domain.exception.FlowStepNotStartedBeforeException;
-
-import java.time.Instant;
 import java.util.Map;
 import java.util.Objects;
 
-public class FlowStep {
-    private final String flowName;
-    private final String stepName;
-    private final ServiceCall serviceCall;
-    private final Map<String, String> requires;
-    private final Map<String, String> export;
-    private Instant startedAt;
-    private Instant finishedAt;
-
-    private FlowStep(
-            String flowName,
-            String stepName,
-            ServiceCall serviceCall,
-            Map<String, String> requires,
-            Map<String, String> export
-    ) {
-        this.flowName = Objects.requireNonNull(flowName, "flowName cannot be null");
-        this.stepName = Objects.requireNonNull(stepName, "stepName cannot be null");
-        this.serviceCall = Objects.requireNonNull(serviceCall, "serviceCall cannot be null");
-        // TODO: make better implementation later
-        this.requires = Objects.isNull(requires) ? null : Map.copyOf(requires);
-        this.export = Objects.isNull(export) ? null : Map.copyOf(export);
-    }
+public record FlowStep(
+        String flowName,
+        String stepName,
+        ServiceCall serviceCall,
+        Map<String, String> requires,
+        Map<String, String> export
+) {
 
     public static FlowStep create(
             String flowName,
@@ -37,60 +18,12 @@ public class FlowStep {
             Map<String, String> requires,
             Map<String, String> export) {
 
-        return new FlowStep(flowName, stepName, serviceCall, requires, export);
-    }
-
-    public void start() {
-        this.startedAt = Instant.now();
-    }
-
-    public void finish() {
-        if (startedAt == null) {
-            throw new FlowStepNotStartedBeforeException(stepName);
-        }
-
-        finishedAt = Instant.now();
-    }
-
-    public String getFlowName() {
-        return flowName;
-    }
-
-    public String getStepName() {
-        return stepName;
-    }
-
-    public ServiceCall getServiceCall() {
-        return serviceCall;
-    }
-
-    public Map<String, String> getRequires() {
-        return requires;
-    }
-
-    public Map<String, String> getExport() {
-        return export;
-    }
-
-    public Instant getStartedAt() {
-        return startedAt;
-    }
-
-    public Instant getFinishedAt() {
-        return finishedAt;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof FlowStep flowStep)) return false;
-
-        return Objects.equals(flowName, flowStep.flowName)
-                && Objects.equals(stepName, flowStep.stepName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(flowName, stepName);
+        return new FlowStep(
+                Objects.requireNonNull(flowName),
+                Objects.requireNonNull(stepName),
+                Objects.requireNonNull(serviceCall),
+                requires == null ? null : Map.copyOf(requires),
+                export == null ? null : Map.copyOf(export)
+        );
     }
 }
