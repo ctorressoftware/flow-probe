c5efb264b2fae3a1d2bf861268782bad2c00aa55
Carlos Torres <ctorressoftware@hotmail.com>
2026-08-30 19:30:26 -0400

    refactor: convert FlowExecutionSummary class to a record

--- src/main/java/io/github/ctorressoftware/domain/model/FlowExecutionSummary.java
+++ src/main/java/io/github/ctorressoftware/domain/model/FlowExecutionSummary.java
@@ -1,43 +1,21 @@
 package io.github.ctorressoftware.domain.model;
 
 import java.util.List;
+import java.util.Objects;
 
-public class FlowExecutionSummary {
-    private String flowName;
-    private boolean successfulExecution;
-    private List<FlowExecutionSummaryDetail> stepsResults;
+public record FlowExecutionSummary(
+        String flowName,
+        boolean successfulExecution,
+        List<FlowExecutionSummaryDetail> stepsResults
+) {
 
     public FlowExecutionSummary(
             String flowName,
             boolean successfulExecution,
             List<FlowExecutionSummaryDetail> stepsResults
     ) {
-        this.flowName = flowName;
+        this.flowName = Objects.requireNonNull(flowName);
         this.successfulExecution = successfulExecution;
-        this.stepsResults = stepsResults;
-    }
-
-    public String getFlowName() {
-        return flowName;
-    }
-
-    public void setFlowName(String flowName) {
-        this.flowName = flowName;
-    }
-
-    public boolean isSuccessfulExecution() {
-        return successfulExecution;
-    }
-
-    public void setSuccessfulExecution(boolean successfulExecution) {
-        this.successfulExecution = successfulExecution;
-    }
-
-    public List<FlowExecutionSummaryDetail> getStepsResults() {
-        return stepsResults;
-    }
-
-    public void setStepsResults(List<FlowExecutionSummaryDetail> stepsResults) {
-        this.stepsResults = stepsResults;
+        this.stepsResults = Objects.requireNonNull(stepsResults);
     }
 }
