804691ff960b88d50c0111d8e0afc5ee88e30d6a
Carlos Torres <ctorressoftware@hotmail.com>
2026-09-01 23:31:37 -0400

    feat: add success and failure static methods in FlowExecutionSummaryDetail record

--- src/main/java/io/github/ctorressoftware/domain/model/FlowExecutionSummaryDetail.java
+++ src/main/java/io/github/ctorressoftware/domain/model/FlowExecutionSummaryDetail.java
@@ -1,11 +1,47 @@
 package io.github.ctorressoftware.domain.model;
 
 import java.time.Duration;
+
 // TODO: Consider adding startedAt and finishedAt fields in the future
+// TODO: add validations
+// TODO: Include expectation validation results in the execution summary
+
 public record FlowExecutionSummaryDetail(
     String stepName,
     boolean successful,
     ServiceCall executed,
     Duration executionDuration,
     String rawResponse
-) {} // TODO: add validations
+) {
+
+    public static FlowExecutionSummaryDetail success(
+            String stepName,
+            ServiceCall executed,
+            Duration executionDuration,
+            String rawResponse
+    ) {
+        return new FlowExecutionSummaryDetail(
+                stepName,
+                true,
+                executed,
+                executionDuration,
+                rawResponse
+        );
+    }
+
+    public static FlowExecutionSummaryDetail failure(
+            String stepName,
+            ServiceCall executed,
+            Duration executionDuration,
+            String rawResponse
+    ) {
+        return new FlowExecutionSummaryDetail(
+                stepName,
+                false,
+                executed,
+                executionDuration,
+                rawResponse
+        );
+    }
+
+}
