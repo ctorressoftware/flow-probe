60fe0e0f56bb390e3e0e10d2f1b010d273d54030
Carlos Torres <ctorressoftware@hotmail.com>
2026-09-01 23:30:17 -0400

    feat: add NotEqualsExpectationEvaluator operator for response validation

--- src/main/java/io/github/ctorressoftware/application/usecase/flowexecution/validation/evaluator/NotEqualsExpectationEvaluator.java
+++ src/main/java/io/github/ctorressoftware/application/usecase/flowexecution/validation/evaluator/NotEqualsExpectationEvaluator.java
@@ -1,4 +1,26 @@
 package io.github.ctorressoftware.application.usecase.flowexecution.validation.evaluator;
 
-public class NotEqualsExpectationEvaluator {
+import io.github.ctorressoftware.application.usecase.flowexecution.validation.result.ExpectationResult;
+import io.github.ctorressoftware.domain.model.ExpectationOperator;
+
+import java.util.Objects;
+
+public class NotEqualsExpectationEvaluator implements ExpectationEvaluator {
+
+    @Override
+    public ExpectationOperator operator() {
+        return ExpectationOperator.NOT_EQUALS;
+    }
+
+    @Override
+    public ExpectationResult evaluate(Object actual, Object expected) {
+
+        boolean successful = !Objects.equals(actual, expected);
+
+        return new ExpectationResult(
+                successful,
+                expected,
+                actual
+        );
+    }
 }
