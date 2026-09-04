34f6631316809b3044fb8d66e99566c34b5557d9
Carlos Torres <ctorressoftware@hotmail.com>
2026-09-03 23:37:55 -0400

    refactor: reconfigure run command removing interactive option

--- src/main/java/io/github/ctorressoftware/infrastructure/cli/RunCommand.java
+++ src/main/java/io/github/ctorressoftware/infrastructure/cli/RunCommand.java
@@ -23,12 +23,14 @@ import java.util.stream.Collectors;
 
 @CommandLine.Command(name = "run")
 public class RunCommand implements Callable<Integer> {
-    private final Scanner scanner;
     private final PrintStream out;
     private final RequestRenderer requestRenderer;
+    private final ReadFileUseCase readFileUseCase;
+    private final ExecuteFlowUseCase executeFlowUseCase;
+    private final CreateImpedimentTicketUseCase createImpedimentTicketUseCase;
 
     @CommandLine.Option(
-            names = {"-f", "--file"},
+            names = {"--file"},
             paramLabel = "FILEPATH",
             description = "Required YAML file path to read it",
             required = true
@@ -36,36 +38,19 @@ public class RunCommand implements Callable<Integer> {
     private String filePath;
 
     @CommandLine.Option(
-            names = {"-i", "--impediment", "--create-impediment"},
-            paramLabel = "IMPEDIMENT",
-            description = "Flag to know if create an impediment or not",
-            fallbackValue = "false",
-            interactive = true
+            names = {"--create-impediment"},
+            description = "Create an impediment when the flow fails"
     )
     private Boolean impedimentCreation;
 
-    @CommandLine.Option(
-            names = {"-r", "--request-format"},
-            paramLabel = "REQUEST_FORMAT",
-            description = "Flag to know the request format to render",
-            fallbackValue = "false"
-    )
-    private final RequestFormat requestFormat = RequestFormat.CURL;
-
-    private final ReadFileUseCase readFileUseCase;
-    private final ExecuteFlowUseCase executeFlowUseCase;
-    private final CreateImpedimentTicketUseCase createImpedimentTicketUseCase;
-
     public RunCommand(
             PrintStream out,
-            Scanner scanner,
             RequestRenderer requestRenderer,
             ReadFileUseCase readFileUseCase,
             ExecuteFlowUseCase executeFlowUseCase,
             CreateImpedimentTicketUseCase createImpedimentTicketUseCase
     ) {
         this.out = out;
-        this.scanner = scanner;
         this.requestRenderer = requestRenderer;
         this.readFileUseCase = readFileUseCase;
         this.executeFlowUseCase = executeFlowUseCase;
@@ -90,11 +75,6 @@ public class RunCommand implements Callable<Integer> {
         ExecuteFlowResult executeFlowResult = executeFlowUseCase.execute(new ExecuteFlowCommand(flow));
         FlowExecutionSummary resume = executeFlowResult.resume();
         renderReproducibleRequests(resume);
-
-        if (!resume.successfulExecution() && Objects.isNull(impedimentCreation)) {
-            out.print("Do you want to create an impediment? (Y/N): ");
-            impedimentCreation = scanner.next().equalsIgnoreCase("Y");
-        }
 
         if (!resume.successfulExecution() && impedimentCreation) {
             ImpedimentTicket ticket = createTicketFromResume(resume);
