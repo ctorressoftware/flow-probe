34eed61395a35de61f48fc96ae698ce9e6931e81
Carlos Torres <ctorressoftware@hotmail.com>
2026-09-05 21:55:17 -0400

    refactor: adjust RequestMapper inner logic

--- src/main/java/io/github/ctorressoftware/infrastructure/callservice/RequestMapper.java
+++ src/main/java/io/github/ctorressoftware/infrastructure/callservice/RequestMapper.java
@@ -2,16 +2,13 @@ package io.github.ctorressoftware.infrastructure.callservice;
 
 import io.github.ctorressoftware.application.exception.JsonSerializationException;
 import io.github.ctorressoftware.application.port.out.JsonProcessor;
-import io.github.ctorressoftware.domain.constant.HttpMethod;
 import io.github.ctorressoftware.domain.exception.HttpServiceCallException;
 import io.github.ctorressoftware.domain.model.ServiceCall;
 
 import java.net.URI;
 import java.net.http.HttpRequest;
 import java.time.Duration;
-import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Stream;
 
 public class RequestMapper {
 
@@ -25,25 +22,21 @@ public class RequestMapper {
 
     public HttpRequest map(ServiceCall request) {
 
-        HttpRequest.BodyPublisher body = request.body() == null ?
-                HttpRequest.BodyPublishers.noBody() :
-                HttpRequest.BodyPublishers.ofString(serializeBody(request.body()));
+        HttpRequest.BodyPublisher body = request.body() == null
+                ? HttpRequest.BodyPublishers.noBody()
+                : HttpRequest.BodyPublishers.ofString(serializeBody(request.body())
+        );
 
-        Map<String, String> headers =
-                request.headers() == null ? Map.of() : request.headers();
-
-        String[] headersArray = headers.entrySet().stream()
-                .flatMap(entry ->
-                        Stream.of(entry.getKey(), String.valueOf(entry.getValue()))
-                )
-                .toArray(String[]::new);
-
-        return HttpRequest.newBuilder()
+        HttpRequest.Builder builder = HttpRequest.newBuilder()
                 .uri(URI.create(request.url()))
-                .headers(headersArray)
                 .method(request.method(), body)
-                .timeout(requestTimeout)
-                .build();
+                .timeout(requestTimeout);
+
+        if (request.headers() != null) {
+            request.headers().forEach(builder::header);
+        }
+
+        return builder.build();
     }
 
     private String serializeBody(Object body) {
